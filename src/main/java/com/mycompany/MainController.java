d229fd1ebb5f0fd11750dad33314b2721bac9038
Vishal Maheshwari <vishalm6683@gmail.com>
2022-01-29 21:21:45 -0500

    Created Home page abd fixed database connection

---
+++ src/main/java/com/mycompany/MainController.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class MainController {
+
+    @GetMapping("")
+    public String showHomepage(){
+        return "index";
+    }
+}
