dde3672736323d028f7332b92bb7278a7570c0be
Vishal Maheshwari <vishalm6683@gmail.com>
2022-01-29 21:21:45 -0500

    Added Boostrap

---
+++ src/main/java/com/mycompany/MainController.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class MainController {
+
+    @GetMapping("")
+    public String showHomepage(){
+
+        return "index";
+    }
+}
--- src/main/java/com/mycompany/MyWebAppApplication.java
+++ src/main/java/com/mycompany/MyWebAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyWebAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         SpringApplication.run(MyWebAppApplication.class, args);
     }
 
